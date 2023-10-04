ab84927115c7c85fac63b72681f8555c7c2c1a2d
Blessings Wandji <120319884+Bledsings@users.noreply.github.com>
2023-10-04 07:35:49 +0100

    Handling Exceptions completed

---
+++ HandlingExeceptions/src/Example1/Apploication.java
@@ -0,0 +1,15 @@
+package Example1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Apploication {
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		File file = new File ("Example.txt");
+		
+		Scanner readFile = new Scanner(file);
+	}
+
+}
---
+++ HandlingExeceptions/src/Example2/Application.java
@@ -0,0 +1,24 @@
+package Example2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+		try {
+			read();
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		}
+
+	}
+
+	public static void read() throws FileNotFoundException {
+		File file = new File("Example.txt");
+
+		Scanner readFile = new Scanner(file);
+
+	}
+}
